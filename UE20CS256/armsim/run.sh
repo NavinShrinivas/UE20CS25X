f08fe3ac74ebba55ec214b0c8877d3cbffc544f2
P K Navin Shirnivas <karupal2002@gmail.com>
2022-01-18 16:33:18 +0530

    updates to the script
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>
    
    I've added supposrt for both 32 and 64 bit systems

--- UE20CS256/armsim/run.sh
+++ UE20CS256/armsim/run.sh
@@ -1,12 +1,29 @@
-if [ -f "./arm-none-eabi-as" ]
+if [ -f "./ARMSim.exe" ]
 then
-    echo `mono ./ARMSim.exe` > /dev/null
-    if [ $? -gt "0" ]
-    then
-        echo "Doesn't seem like you have mono isntalled, do it."
-    else
-        mono ./ARMSim.exe
-    fi
+        echo "What architechture does your system follow?"
+        echo "1.64 Bit"
+        echo "2.32 Bit"
+        echo  -n "Enter option : "
+        read option
+        if [ -f "./arm-none-eabi-as" ];then
+            mono ./ARMSim.exe >  /dev/null 2>&1
+        else
+            if [ $option = "1" ];then
+                cp ./assemblers/arm-none-eabi-as-64 ./
+                mv ./arm-none-eabi-as-64 ./arm-none-eabi-as
+                chmod +x ./arm-none-eabi-as
+                mono ./ARMSim.exe > /dev/null 2>&1
+            else
+                cp ./assemblers/arm-none-eabi-as-32 ./
+                mv ./arm-none-eabi-as-32 ./arm-none-eabi-as
+                chmod +x ./arm-none-eabi-as
+                mono ./ARMSim.exe > /dev/null 2>&1
+            fi
+       fi
+        if [ $? -gt "0" ];then
+            echo "Seems like you dont have mono installed"
+        fi
+ 
 else
-    echo "You have messed up somehow and deleted some importatn files.reclone"
+    echo "You have messed up somehow and deleted some importatn files, reclone"
 fi
