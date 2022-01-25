40dec4a5c821cf446fbdca14e7c5113721da8ff5
P K Navin Shirnivas <karupal2002@gmail.com>
2022-01-25 14:47:26 +0530

    Adding a neattt build system
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>

---
+++ UE20CS255-CNLAB/packettracer/install.sh
@@ -0,0 +1,33 @@
+echo "Which distro are u running me on?"
+echo "1.Debian base distros [Ubunut, pop, ...]"
+echo "2.Arch base distro [EOS, Manjaro, ..]"
+echo -n "Enter option :"
+read option
+
+if [ $option -ne "1" ];then
+    sudo pacman -S wget
+    wget "https://downloads.sourceforge.net/project/packettracr/CiscoPacketTracer_810_Ubuntu_64bit.deb?ts=gAAAAABh77m3GHnlK3EL5pN7AQqChelZUKAWjB1OMqx36dJdqmU0pn4bWv9ti0slhf_Uvfg2lHsk-Fk2eZtlcn3cqPSqETAvCQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fpackettracr%2Ffiles%2FCiscoPacketTracer_810_Ubuntu_64bit.deb%2Fdownload"
+    mv ./CiscoPacketTracer_810_Ubuntu_64bit.deb\?ts=gAAAAABh77m3GHnlK3EL5pN7AQqChelZUKAWjB1OMqx36dJdqmU0pn4bWv9ti0slhf_Uvfg2lHsk-Fk2eZtlcn3cqPSqETAvCQ==\&r=https:\%2F\%2Fsourceforge.net\%2Fprojects\%2Fpackettracr\%2Ffiles\%2FCiscoPacketTracer_810_Ubuntu CiscoPacketTracer_810_Ubuntu_64bit.deb    
+    makepkg -si
+    if [ $? -ne "0" ];then
+        echo "oh oh, something went wrong...may be wrong distro?"
+        exit
+    fi
+else
+    sudo apt install gdebi
+    if [ $? -ne "0" ];then
+        echo "oh oh, something went wrong, are you a weirdo running debian base with no apt?"
+        exit
+    else
+        sudo apt-get install wget
+        wget "https://downloads.sourceforge.net/project/packettracr/CiscoPacketTracer_810_Ubuntu_64bit.deb?ts=gAAAAABh77m3GHnlK3EL5pN7AQqChelZUKAWjB1OMqx36dJdqmU0pn4bWv9ti0slhf_Uvfg2lHsk-Fk2eZtlcn3cqPSqETAvCQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fpackettracr%2Ffiles%2FCiscoPacketTracer_810_Ubuntu_64bit.deb%2Fdownload"
+         mv ./CiscoPacketTracer_810_Ubuntu_64bit.deb\?ts=gAAAAABh77m3GHnlK3EL5pN7AQqChelZUKAWjB1OMqx36dJdqmU0pn4bWv9ti0slhf_Uvfg2lHsk-Fk2eZtlcn3cqPSqETAvCQ==\&r=https:\%2F\%2Fsourceforge.net\%2Fprojects\%2Fpackettracr\%2Ffiles\%2FCiscoPacketTracer_810_Ubuntu CiscoPacketTracer_810_Ubuntu_64bit.deb
+        sudo gdebi ./CiscoPacketTracer_810_Ubuntu_64bit.deb
+        if [ $? -ne "0" ];then
+            echo "damn those dependencies"
+            sudo apt install -f
+            sudo gdebi ./CiscoPacketTracer_810_Ubuntu_64bit.deb
+        fi
+    fi
+    echo "Everything went alright, you should be good!"
+fi
---
+++ UE20CS255-CNLAB/packettracer/packettracer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PT8HOME=/opt/packettracer/
+export PT8HOME
+
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+QT_VERSION=`qmake -v | grep "Using Qt version" | sed -r 's/.* ([0-9]+\.*[0-9]+\.*[0-9]*).*?/\1/'`
+QT_VERSION_CHECK="5.5.0"
+
+if version_gt $QT_VERSION $QT_VERSION_CHECK;then
+QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_AUTO_SCREEN_SCALE_FACTOR
+else
+QT_DEVICE_PIXEL_RATIO=auto
+export QT_DEVICE_PIXEL_RATIO
+fi
