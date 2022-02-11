ae3171eb54b487c1f3932dba518eee258292d952
P K Navin Shirnivas <karupal2002@gmail.com>
2022-02-11 08:01:38 +0530

    adding sources from yesterdays cn lab

---
+++ UE20CS255-CNLAB/week-2_3/src/index.php
@@ -0,0 +1,9 @@
+<html>
+    <?php
+        setcookie("SRN","PES2UG20CS237");
+        setcookie("Name","Navin Shrinivas",time()+125);
+    ?>
+        <body>
+        <h1>Hello world</h1>
+        </body>
+</html>
